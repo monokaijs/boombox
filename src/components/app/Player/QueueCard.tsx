bed22146e5f4dd27645c1b92856757aae8430806
MonokaiJs <monokaijs@gmail.com>
2023-05-25 21:07:27 +0700

    :sparkles: prevent queue overflow

--- src/components/app/Player/QueueCard.tsx
+++ src/components/app/Player/QueueCard.tsx
@@ -19,50 +19,52 @@ export default function QueueCard() {
       <EnqueueInput/>
     </>}
   >
-    <List
-      dataSource={queue}
-      renderItem={item => (
-        <List.Item className={styles.queueItem}>
-          <img
-            src={item.picture}
-            className={styles.queueItemPicture}
-          />
-          <div className={styles.queueItemName}>
-            <div>
-              {item.title}
+    <div className={styles.listWrapper}>
+      <List
+        dataSource={queue}
+        renderItem={item => (
+          <List.Item className={styles.queueItem}>
+            <img
+              src={item.picture}
+              className={styles.queueItemPicture}
+            />
+            <div className={styles.queueItemName}>
+              <div>
+                {item.title}
+              </div>
+              <Tag>
+                {moment.utc(item.duration * 1000).format(
+                  item.duration > 3600 ? 'HH:mm:ss' : 'mm:ss'
+                )}
+              </Tag>
             </div>
-            <Tag>
-              {moment.utc(item.duration * 1000).format(
-                item.duration > 3600 ? 'HH:mm:ss' : 'mm:ss'
-              )}
-            </Tag>
-          </div>
-          <div className={styles.controls}>
-            <Button shape={'circle'} type={'text'} danger onClick={() => {
-              dispatch(removeTrack(item.id));
-              peerService.sendAll(encodeURIComponent(JSON.stringify({
-                action: 'remove-track',
-                data: item.id,
-              })));
-            }}>
-              <DeleteOutlined/>
-            </Button>
-            <Button shape={'circle'} type={'text'} onClick={() => {
-              const newState = {
-                currentTrack: item.id,
-                currentTrackTime: 0
-              };
-              dispatch(syncPlayer(newState));
-              peerService.sendAll(encodeURIComponent(JSON.stringify({
-                action: 'sync-player',
-                data: newState
-              })));
-            }}>
-              <PlayCircleOutlined/>
-            </Button>
-          </div>
-        </List.Item>
-      )}
-    />
+            <div className={styles.controls}>
+              <Button shape={'circle'} type={'text'} danger onClick={() => {
+                dispatch(removeTrack(item.id));
+                peerService.sendAll(encodeURIComponent(JSON.stringify({
+                  action: 'remove-track',
+                  data: item.id,
+                })));
+              }}>
+                <DeleteOutlined/>
+              </Button>
+              <Button shape={'circle'} type={'text'} onClick={() => {
+                const newState = {
+                  currentTrack: item.id,
+                  currentTrackTime: 0
+                };
+                dispatch(syncPlayer(newState));
+                peerService.sendAll(encodeURIComponent(JSON.stringify({
+                  action: 'sync-player',
+                  data: newState
+                })));
+              }}>
+                <PlayCircleOutlined/>
+              </Button>
+            </div>
+          </List.Item>
+        )}
+      />
+    </div>
   </Card>
 }
